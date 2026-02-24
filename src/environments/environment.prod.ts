3604470ac0214aaf52ad9a91464a375159295eba
JoniMeichtry11 <joni.meichtry@gmail.com>
2026-02-24 14:53:03 -0300

    feat: add backend url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,10 +8,6 @@ export const environment = {
     messagingSenderId: "415399502496",
     appId: "1:415399502496:web:20f5c73916a7a905f31fc5"
   },
-  mercadopago: {
-    // El token ahora se maneja en el servidor Node.js por seguridad
-  },
-  backendUrl: 'https://pives-backend.onrender.com' // Cambiá esto cuando crees el servicio en Render
+  mercadopago: {},
+  backendUrl: 'https://p-i-v-e-s-proyect.onrender.com'
 };
-
-
